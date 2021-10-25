ba4080c5e263825f136f064248a1476efdc5f38c
Dheeraj <dheeraj.p@moonraft.com>
2021-10-25 14:32:30 +0530

    latest code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { ButtonComponent } from './button/button.component';
-import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
-import { HeroBannerComponent } from './hero-banner/hero-banner.component';
+import { HeaderComponent } from './component/header/header.component';
+import { NavigationComponent } from './component/navigation/navigation.component';
+import { ButtonComponent } from './component/button/button.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { MainComponent } from './component/main/main.component';
+import { HeroBannerComponent } from './component/hero-banner/hero-banner.component';
+import { QuickLinksComponent } from './component/quick-links/quick-links.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { HeroBannerComponent } from './hero-banner/hero-banner.component';
     ButtonComponent,
     FooterComponent,
     MainComponent,
-    HeroBannerComponent
+    HeroBannerComponent,
+    QuickLinksComponent
   ],
   imports: [
     BrowserModule,
